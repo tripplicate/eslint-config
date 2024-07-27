166672766c6895f187a274a027420f96f4630e22
Danil Sergeev <tripplicate.me@gmail.com>
2024-07-27 21:41:53 +0300

    chore: update lint-staged configuration

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,3 +1,3 @@
 module.exports = {
-  'packages/**/*.{js,json,yaml,yml,md}': "pnpm exec nx affected -t eslint --files"
+  'packages/**/*.{js,json,yaml,yml,md}': "pnpm exec nx affected -t eslint --fix --files"
 }
